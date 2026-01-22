2ed96ad2eca662fc789939dbc1a43b0015672dd0
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2026-01-22 17:39:38 +0100

    fix end of study and repetition avoidance

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,77 @@
-use std::{error::Error, thread, time::{ Duration, Instant}};
+use std::{
+    error::Error,
+    thread,
+    time::{Duration, Instant},
+};
 
 use reqwest::blocking::{self};
 
-
 fn main() {
     let start = Instant::now();
-    let study_interval = Duration::from_secs(5*60); // 5 minutes
-    let total_elapsed = start.elapsed();
-    let snapshot_interval = Duration::from_secs(60*2); // 2 minutes
-    
-    let mut last_downloaded = "";
+    let study_interval = Duration::from_secs(5 * 60); // 5 minutes
+    let snapshot_interval = Duration::from_secs(60 * 2); // 2 minutes
+
+    let mut last_downloaded = "".to_string();
 
     let url = "https://traficosevilla.es/camaras/cam28.jpg";
     loop {
         let loop_start = Instant::now();
-        
+
         // ---- Task start ----
-        println!("Running task at {:?}", loop_start);
-        
-        let _ = fetch_image(url, &last_downloaded).inspect(|new| last_downloaded = new);
+        println!("\nRunning task at {:?}", loop_start);
+
+        let _ = fetch_image(url, &last_downloaded).inspect(|new| last_downloaded = new.clone());
         // ---- Task end ----
 
         let elapsed = loop_start.elapsed();
+        let total_elapsed = start.elapsed();
 
-        if total_elapsed > study_interval{
+        if total_elapsed > study_interval {
+            println!("\nENDED STUDY INTERVAL");
             break;
         }
         if elapsed < snapshot_interval {
             thread::sleep(snapshot_interval - elapsed);
         } else {
             // If the task took longer than the snapshot_interval, continue immediately.
-            eprintln!("Warning: task took longer ({:?}) than snapshot_interval {:?}", elapsed, snapshot_interval);
+            eprintln!(
+                "Warning: task took longer ({:?}) than snapshot_interval {:?}",
+                elapsed, snapshot_interval
+            );
         }
     }
 }
-
 
 // fetches the image url, and checks the asociated date, if date!=last_downloaded image is saved to file system and returns ok
-fn fetch_image<'a>(image_url:&str, last_downloaded:&str) -> Result<&'a str, Box<dyn Error>>{
-    //request.send().inspect(|res| save_if_not_present(res,id));
-    
+fn fetch_image<'a>(image_url: &str, last_downloaded: &str) -> Result<String, Box<dyn Error>> {
+    print!("requesting data");
     let resp = blocking::get(image_url)?;
-    let date = "";
+    let date: String;
     if resp.status().is_success() {
-        //let date = resp?.headers().get("date");//extract the header "date" as &str
-/*         if let Some(value) = resp.headers().get("date") {
-            date = value.to_str()?.clone(); // returns &str or Err if not valid UTF-8
-            println!("date: {}", date);
-        } else {
-            println!("<date>Header not present");
-        } */
-        let date = resp
-        .headers()
-        .get("date")
-        .and_then(|v| v.to_str().ok())
-        .map(|s| s.to_owned())
-        .unwrap_or_default(); // take an owned String
+        date = resp
+            .headers()
+            .get("last-modified")
+            .and_then(|v| v.to_str().ok())
+            .map(|s| s.to_owned())
+            .unwrap_or_default(); // take an owned String
 
-        if  date != last_downloaded { // if the date of the image is not the same as the last one, save it.
-            //last_downloaded = &date;
+        print!(" => succesfull http request!\n");
+
+        if date != last_downloaded {
+            // if the date of the image is not the same as the last one, save it.
             let img_bytes = resp.bytes()?; // consume response
-            //let img_bytes = res.bytes().unwrap_or_default();
             let image = image::load_from_memory(&img_bytes).unwrap_or_default();
-            let _ = image.save_with_format(format!("./images/{:?}.webp", date) , image::ImageFormat::WebP);
+            let path = format!("./images/{:?}.webp", date.clone());
+            //let path = "../images/test.webp";
+            let _ = image.save_with_format(path.clone(), image::ImageFormat::WebP);
+            println!("new image at {}", path);
+            Ok(date.clone())
+        } else {
+            Ok(date.clone())
         }
     } else {
-        eprintln!("Request failed: {}", resp.status());
+        let msg = format!("Request failed: {}", resp.status());
+        eprintln!("{}", msg);
+        Err(msg.into())
     }
-
-    Ok(&date)
-        
 }
-
