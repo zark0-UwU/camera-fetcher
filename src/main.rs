4641a55ee948e84b2b426eefbf0f98bf89cab6d8
zark0 <dev.zark0@gmail.com>
2026-03-03 16:50:55 +0100

    ✨ Added multi camera support

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,53 @@ use chrono::{NaiveDateTime, Datelike};
 
 use reqwest::blocking::{self};
 
+#[derive(Clone)]
+struct Camera {
+    url: String,
+    name: String,
+    last_downloaded: String,
+}
+
+impl Camera {
+/*     fn new_from_name(camera_name: &str) -> Camera{
+            Camera {
+            url: format!("https://trafico.sevilla.org/camaras/{}.jpg", camera_name).to_string(),
+            name: camera_name.to_string(),
+            last_downloaded: String::from(""),
+        }
+    }
+    fn new_from_id(camera_id: i16) -> Camera{
+            Camera {
+            url: format!("https://trafico.sevilla.org/camaras/cam{}.jpg", camera_id).to_string(),
+            name: format!("cam{}",camera_id).to_string(),
+            last_downloaded: String::from(""),
+        }
+    } */
+    fn new(url: &str, name: &str) -> Camera{
+            Camera {
+            url: url.to_string(),
+            name: name.to_string(),
+            last_downloaded: String::from(""),
+        }
+    }
+}
+
 fn main() {
+    
+    let mut cameras = [
+        Camera::new("https://trafico.sevilla.org/camaras/cam28.jpg", "Plaza de cuba (puente de san telmo)"),
+        Camera::new("https://trafico.sevilla.org/camaras/cam9.jpg", "Puente Isabel II - Paseo Colon - Arjona"),
+        Camera::new("https://trafico.sevilla.org/camaras/cam73.jpg", "Puerta de la carne - Menendez y Pelayo"),
+        Camera::new("https://trafico.sevilla.org/camaras/cam26.jpg", "Av de la Palmera - Bueno Monreal"),
+        Camera::new("https://trafico.sevilla.org/camaras/cam88.jpg", "Kansas city - Luis Montoto"),
+        Camera::new("https://trafico.sevilla.org/camaras/cam23.jpg", "Glorieta de Santa Justa")
+
+    ];
+
     let start = Instant::now();
     let study_interval = Duration::from_secs(24 * 60 * 60); // 24h minutes
     let endless_study = true; // ignores study interval making the script run continusly
     let snapshot_interval = Duration::from_secs(60 * 2); // every 2 minutes
-    let url = "https://trafico.sevilla.org/camaras/cam28.jpg"; // using this cammera
-
-    let mut last_downloaded = "".to_string();
 
     println!("Endless mode: {}", endless_study);
 
@@ -20,9 +59,12 @@ fn main() {
         let loop_start = Instant::now();
 
         // ---- Task start ----
-        println!("\nRunning task at {:?}", loop_start);
-
-        let _ = fetch_image(url, &last_downloaded).inspect(|new| last_downloaded = new.clone());
+        let mut cam_index = 0;
+        while cam_index < cameras.len(){
+            println!("\nRunning task at {:?}", loop_start);
+            let _ = fetch_image(cameras[cam_index].clone()).inspect(|new| cameras[cam_index].last_downloaded = new.clone());
+            cam_index += 1;
+        }
         // ---- Task end ----
 
         let elapsed = loop_start.elapsed();
@@ -45,9 +87,9 @@ fn main() {
 }
 
 // fetches the image url, and checks the asociated date, if date!=last_downloaded image is saved to file system and returns ok
-fn fetch_image<'a>(image_url: &str, last_downloaded: &str) -> Result<String, Box<dyn Error>> {
+fn fetch_image<'a>(camera: Camera) -> Result<String, Box<dyn Error>> {
     print!("requesting data");
-    let resp = blocking::get(image_url)?;
+    let resp = blocking::get(camera.url)?;
     let date: String;
     if resp.status().is_success() {
         date = resp
@@ -59,7 +101,7 @@ fn fetch_image<'a>(image_url: &str, last_downloaded: &str) -> Result<String, Box
 
         print!(" => succesfull http request!\n");
 
-        if date != last_downloaded {
+        if date != camera.last_downloaded {
             // if the date of the image is not the same as the last one, save it.
             let img_bytes = resp.bytes()?; // consume response
             let image = image::load_from_memory(&img_bytes).unwrap_or_default();
@@ -73,17 +115,21 @@ fn fetch_image<'a>(image_url: &str, last_downloaded: &str) -> Result<String, Box
                 let month = parsed_utc.month();
                 let day = parsed_utc.day();
 
+                // make file name compliant with file system restrictions
                 let new_filename = format!("{}.webp",parsed_utc.to_rfc3339_opts(chrono::SecondsFormat::Secs, false).replace(":", "."));
                 
-                let str_path = format!("./images/{}/{}", month, day);
+                // construct the file path
+                let str_path = format!("./images/{}/{}/{}", camera.name, month, day);
                 let folder_path =  Path::new(&str_path);
-                //if folder does not 
+                
+                //if folder does not exist create it
                 if folder_path.try_exists().is_ok_and(|v| !v){
                     let _ = fs::create_dir_all(folder_path);
                 }
                 
                 let file_path = folder_path.join(new_filename);
                 
+                // Save image file
                 let _ = image.save_with_format(file_path.clone(), image::ImageFormat::WebP);
                 println!("new image at {}", file_path.to_string_lossy());
             }
